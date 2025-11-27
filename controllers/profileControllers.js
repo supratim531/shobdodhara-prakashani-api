dd49734eb1a30b1dfcf4ee11249d3603abbd866c
supratim531 <supratimm531@gmail.com>
2025-11-27 06:12:38 +0530

    supratim531: fix(profileControllers): Added addresses with user details in current profile endpoint

--- controllers/profileControllers.js
+++ controllers/profileControllers.js
@@ -1,3 +1,4 @@
+import Address from "../models/addressModel.js";
 import { successResponse } from "../utils/response.js";
 import expressAsyncHandler from "express-async-handler";
 
@@ -7,7 +8,12 @@ import expressAsyncHandler from "express-async-handler";
  * @access private
  */
 const currentProfileController = expressAsyncHandler(async (req, res) => {
-  return successResponse(res, "Current profile fetched", req.user);
+  const user = req.user;
+  user.addresses = await Address.find({ userId: req.user.id })
+    .select("-_id -user_id -createdAt -updatedAt -__v")
+    .lean();
+
+  return successResponse(res, "Current profile fetched", user);
 });
 
 export { currentProfileController };

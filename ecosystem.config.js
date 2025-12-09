2396698e6ab00eace16f41abed75784a06ce4cd2
supratim531 <supratimm531@gmail.com>
2025-12-09 20:07:07 +0530

    supratim531: feat(ecosystem.config): added to support pm2 - a node process manager to run the application with the corresponding .env file

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+export default {
+  apps: [
+    {
+      name: "shobdodhara",
+      script: "server.js",
+      env: {
+        NODE_ENV: "development",
+        ENV_FILE: ".env.development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+        ENV_FILE: ".env.production",
+      },
+    },
+  ],
+};

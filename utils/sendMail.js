d28fb33c83ce1e5ab4782f45629355bfa2795954
supratim531 <supratimm531@gmail.com>
2025-12-10 18:10:05 +0530

    supratim531: made send email feature to work with non secure protocol and modified the pm2 command in deploy.yml

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -4,9 +4,13 @@ import nodemailer from "nodemailer";
 dotenv.config({ path: "./.env", quiet: true });
 
 const transporter = nodemailer.createTransport({
+  // host: process.env.SMTP_HOST || "smtp.gmail.com",
+  // port: Number(process.env.SMTP_PORT) || 587, // 465 or 587
+  // secure: Number(process.env.SMTP_PORT) === 587 ? false : true, // true for 465, false for 587
   host: process.env.SMTP_HOST || "smtp.gmail.com",
-  port: Number(process.env.SMTP_PORT) || 587, // 465 or 587
-  secure: Number(process.env.SMTP_PORT) === 587 ? false : true, // true for 465, false for 587
+  port: 587,
+  secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,

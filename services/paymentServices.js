9096a7e784382c33377d17839f27286a7d5349b5
supratim531 <supratimm531@gmail.com>
2026-02-23 15:45:27 +0530

    refactor(payment): disabled the actual shipping order creation

--- services/paymentServices.js
+++ services/paymentServices.js
@@ -74,25 +74,25 @@ const verifyPayment = async (
     shippingAddress,
   );
 
-  // Create Shiprocket order
-  const shiprocketOrder = await createShiprocketOrder(order._id, "Prepaid");
+  // // Create Shiprocket order
+  // const shiprocketOrder = await createShiprocketOrder(order._id, "Prepaid");
 
-  // Assign courier for the order
-  const assignedCourier = await assignCourier(
-    order._id,
-    shiprocketOrder.shipment_id,
-    courierId,
-    shiprocketOrder.status,
-  );
+  // // Assign courier for the order
+  // const assignedCourier = await assignCourier(
+  //   order._id,
+  //   shiprocketOrder.shipment_id,
+  //   courierId,
+  //   shiprocketOrder.status,
+  // );
 
-  // Schedule pickup (MOST IMPORTANT)
-  const scheduledPickup = await schedulePickup(
-    order._id,
-    shiprocketOrder.shipment_id,
-  );
+  // // Schedule pickup (MOST IMPORTANT)
+  // const scheduledPickup = await schedulePickup(
+  //   order._id,
+  //   shiprocketOrder.shipment_id,
+  // );
 
-  return { order, shiprocketOrder, assignedCourier, scheduledPickup };
-  return { order, shiprocketOrder };
+  // return { order, shiprocketOrder, assignedCourier, scheduledPickup };
+  // return { order, shiprocketOrder };
   return { order };
 };
 

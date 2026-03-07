2d8effe4bf575862e6091d083b40ffc307c38f05
supratim531 <supratimm531@gmail.com>
2026-03-07 18:44:20 +0530

    fix(sort): fixed several sort parameters

--- utils/sort.js
+++ utils/sort.js
@@ -1,19 +1,21 @@
 export const PRODUCT_SORT_MAP = {
   // Default will be newest
   // Front end will only send one sort parameter, the data will be sorted based on strict one parameter only
-  oldest: { createdAt: 1 },
-  newest: { createdAt: -1 },
+  oldest: { publishedAt: 1 },
+  newest: { publishedAt: -1 },
   priceAsc: { price: 1, _id: 1 },
   priceDesc: { price: -1, _id: -1 },
+  bestselling: { totalSales: -1, _id: -1 },
 };
 
 export const BOOK_SORT_MAP = {
   // Default will be newest
   // Front end will only send one sort parameter, the data will be sorted based on strict one parameter only
-  oldest: { createdAt: 1 },
-  newest: { createdAt: -1 },
+  oldest: { "product.publishedAt": 1 },
+  newest: { "product.publishedAt": -1 },
   priceAsc: { "product.price": 1, _id: 1 },
   priceDesc: { "product.price": -1, _id: -1 },
+  bestselling: { "product.totalSales": -1, _id: -1 },
 };
 
 export const CLOTHES_SORT_MAP = {
@@ -23,4 +25,5 @@ export const CLOTHES_SORT_MAP = {
   newest: { createdAt: -1 },
   priceAsc: { "product.price": 1, _id: 1 },
   priceDesc: { "product.price": -1, _id: -1 },
+  bestselling: { "product.totalSales": -1, _id: -1 },
 };
